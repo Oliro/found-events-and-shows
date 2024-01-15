95b89109c5e409ad8dd1c187049922026ec84042
Fernando OR <fernando.or6@gmail.com>
2024-01-15 13:20:27 -0300

    feat: Implement search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.sass'
 })
 export class SearchComponent {
 
+  public query: string = '';
+
+  @Output() public queryEvent = new EventEmitter<string>();
+
+  search() {
+    this.queryEvent.emit(this.query);
+  }
+
 }
+
