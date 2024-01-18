1ef2b0abb8deb2452e67e0eb024641709b9569ed
Fernando OR <fernando.or6@gmail.com>
2024-01-18 10:03:55 -0300

    feat: implement pagination

--- src/app/view-result/view-result.component.ts
+++ src/app/view-result/view-result.component.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { ticketMasterApiService } from '../services/ticketMasterApi.service';
 import { Attraction } from '../models/ticketMaster';
 import { CardsComponent } from '../cards/cards.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaginationPipe } from '../pipes/pagination.pipe';
 
 @Component({
   selector: 'app-view-result',
   standalone: true,
-  imports: [CommonModule, CardsComponent],
+  imports: [CommonModule, NgbModule, CardsComponent, PaginationPipe],
   templateUrl: './view-result.component.html',
   styleUrl: './view-result.component.sass'
 })
@@ -21,6 +23,10 @@ export class ViewResultComponent implements OnChanges {
 
   private ticketMasterApiService = inject(ticketMasterApiService);
 
+  public currentPage: number = 1;
+  public contentPerPage: number = 8;
+  public collectionSize: number = 0;
+
   ngOnChanges(): void {
 
     if (this.query !== undefined) {
@@ -30,9 +36,22 @@ export class ViewResultComponent implements OnChanges {
         })
       )
 
-      this.items.subscribe(x => console.log(x))
+      this.items.subscribe(result => {
+        
+        console.log(result);
+        this.getCollectionSize(result);
+
+      })
     };
 
+  }
+
+  getCollectionSize(result: any) {
+    this.collectionSize = result.length;
+  }
+
+  onPageChange(page: number){
+    //console.log(page);
   }
 
 }
